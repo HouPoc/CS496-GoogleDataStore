a4baff98e5e39255d1e5b302ddd4a5ecee82f6ae
HouPoc <hou199488@gmail.com>
2017-01-26 04:37:01 +0800

    alpha test

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+from google.appengine.ext import ndb
+import json
+import ndb_definition
 import webapp2
 
 
@@ -20,7 +9,19 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class BookHandler(webapp2.RequestHandler):		#Handlers for actions related to book
+    def post(self):					#Hnadlers for post requests
+        book_data = json.loads(self.request.body)	#load the data
+	self.response.write(json.dumps(book_data)
+#class CustomerHandler(webapp2.RequestHandler):
+#   def post(self):
+        
+        
 
+allowed_methods = webapp2.WSGIApplication.allowed_methods
+new_allowed_methods = allowed_methods,union(('PATCH'))
+webapp2.WSGIApplication.allowed_methods = new allowed_methods
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage)
+    ('/book'BookHandler),
 ], debug=True)
