e954a09210626c2bfbf4a2b466d33c71f5caae23
HouPoc <hou199488@gmail.com>
2017-01-26 06:06:26 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,9 +15,6 @@ class BookHandler(webapp2.RequestHandler):		#Handlers for actions related to boo
 	self.response.write(json.dumps(book_data))
 #class CustomerHandler(webapp2.RequestHandler):
 #   def post(self):
-        
-        
-
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
