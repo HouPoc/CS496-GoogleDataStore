fb6805684bb568337f311744d2c92b69b0a76a6d
HouPoc <hou199488@gmail.com>
2017-01-25 19:25:55 -0800

    First test post

--- src/main.py
+++ src/main.py
@@ -12,7 +12,19 @@ class MainPage(webapp2.RequestHandler):
 class BookHandler(webapp2.RequestHandler):		#Handlers for actions related to book
     def post(self):					#Hnadlers for post requests
         book_data = json.loads(self.request.body)	#load the data
-	self.response.write(json.dumps(book_data))
+        new_book = Books(
+            book_id = Books.all(keys_only=True).count() + 1,
+            title = book_data['title'],
+            isbn = book_data['isbn'],
+            genre = book_data['genre'],
+            author = book_data['author'],
+            check_in = book_data['check_in']
+        )
+        new_book.put()
+        book_dict = new_book.to_dict()
+	self.response.write(json.dumps(book_dict))
+
+
 #class CustomerHandler(webapp2.RequestHandler):
 #   def post(self):
 allowed_methods = webapp2.WSGIApplication.allowed_methods
